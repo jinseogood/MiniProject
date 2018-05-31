b694e65f397792e1edaefe5f01a3439b9d26f18e
user1 <user1@KH_B>
2018-05-31 13:11:58 +0900

    배경

--- src/project/view/GameView.java
+++ src/project/view/GameView.java
@@ -1,14 +1,24 @@
 package project.view;
 
 import java.awt.Color;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class GameView extends JPanel{
 	public GameView(){
-		this.setSize(1138, 462);
-		this.setBackground(Color.CYAN);
+		this.setSize(1138, 500);
+		
+		Image icon = new ImageIcon("images/back.PNG").getImage().getScaledInstance(1138, 462, 100); //이미지아이콘으로만드는것이좀더편함
+		
+		JLabel label = new JLabel(new ImageIcon(icon));
+		
+		label.setSize(1138, 500);
+		//label.setLayout(null);
+		
+		this.add(label);
 	}
 	
 }
