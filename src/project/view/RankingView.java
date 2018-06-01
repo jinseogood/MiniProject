d0c498639c76ee26e232a7e92091560857eab761
ijoo <ijoo@DESKTOP-IJOO>
2018-06-01 14:22:28 +0900

    601 soojunghaetda

--- src/project/view/RankingView.java
+++ src/project/view/RankingView.java
@@ -1,31 +1,41 @@
 package project.view;
 
-import java.awt.Color;
 import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class RankingView extends JPanel {
-	private JButton btnReturn;
+	
 	private JPanel panel;
+	private JLabel btnReturn;
 	
 	public RankingView(MainFrame mf, MainMenu m){
 		this.setSize(1138, 500);
 		
-		Image icon = new ImageIcon("images/back.PNG").getImage().getScaledInstance(1138, 462, 100); //이미지아이콘으로만드는것이좀더편함
+		Image icon = new ImageIcon("images/rankingbg.PNG").getImage().getScaledInstance(1138, 462, 100); //이미지아이콘으로만드는것이좀더편함
 		
 		JLabel label = new JLabel(new ImageIcon(icon));
 		
 		label.setSize(1138, 462);
 		//label.setLayout(null);
 		
-		btnReturn=new JButton();
+		Image iReturn = new ImageIcon("Images/buttons/returnbtn.png").getImage().getScaledInstance(218, 121, 0);
+		Image iReturn1 = new ImageIcon("Images/buttons/return.gif").getImage().getScaledInstance(218, 121, 0);
 		
+		btnReturn = new JLabel();
+		
+		btnReturn.setIcon(new ImageIcon(iReturn));
+		
+		btnReturn.setSize(218, 121);
+		btnReturn.setLocation(480, 290);
+		
+		
+		//btnReturn=new JButton();
+		/*
 		ImageIcon btn = new ImageIcon("images/button.PNG");
 		btnReturn.setIcon(btn);
 		
@@ -42,12 +52,32 @@ public class RankingView extends JPanel {
 				mf.repaint();
 			}
 			
-		});
+		});*/
 		
+		
+		btnReturn.addMouseListener(new MouseAdapter() {
+			
+			@Override
+			public void mouseEntered(MouseEvent e){
+				btnReturn.setIcon(new ImageIcon(iReturn1));
+			}
+			@Override
+			public void mouseExited(MouseEvent e){
+				btnReturn.setIcon(new ImageIcon(iReturn));
+			}
+			@Override
+			public void mouseClicked(MouseEvent e){
+				mf.remove(m.getPanel());
+				panel=new MainMenu(mf);
+				mf.add(panel);
+				mf.repaint();
+			}
+			
+		}); 
 		
 		label.add(btnReturn);
 		this.add(label);
-	}
+	} 
 
 	
 
