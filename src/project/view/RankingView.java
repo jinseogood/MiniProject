4d1309149a9017fce93293ad493a00e138f6faff
Administrator <Administrator@JINSEO-NT>
2018-06-05 12:18:02 +0900

    test

--- src/project/view/RankingView.java
+++ src/project/view/RankingView.java
@@ -17,7 +17,7 @@ public class RankingView extends JPanel {
 		RankingView r=this;
 		this.setSize(1138, 500);
 		
-		Image icon = new ImageIcon("images/rankingbg.PNG").getImage().getScaledInstance(1138, 462, 100); //이미지아이콘으로만드는것이좀더편함
+		Image icon = new ImageIcon("images/back.PNG").getImage().getScaledInstance(1138, 462, 100); //이미지아이콘으로만드는것이좀더편함
 		
 		JLabel label = new JLabel(new ImageIcon(icon));
 		
